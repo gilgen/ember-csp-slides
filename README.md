3045b7832f2d0d33767a552c0178e04b2a877e54
Jamie Gilgen <jamie@gilgen.ca>
2019-04-21 10:03:25 -0700

    Drop.

---
+++ README.md
@@ -0,0 +1,10 @@
+## Packaging Ember Libraries
+
+Some slides to go through my method (as at Jan 8, 2014) of packaging Ember libs.
+
+View the deck here: [http://jamiebikies.github.io/packaging-ember-libs](http://jamiebikies.github.io/packaging-ember-libs)
+
+![deets](http://snappys.s3.amazonaws.com/deets.jpg)
+
+
+
---
+++ slide_config.js
@@ -0,0 +1,25 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Packaging Ember Libraries',
+    subtitle: 'ES6, grunt, travis, and mocha galore!',
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    // theme: ['mytheme']
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Jamie Gilgen',
+    company: 'Crowdmark',
+    twitter: '@jamiebikies',
+    github: 'http://github.com/you'
+  }]
+};
+
